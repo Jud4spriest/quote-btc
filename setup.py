f65eea469769d978d5959d472b35693c9e5b691f
x <x>
2025-04-12 15:41:57 -0300

    fix3 setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='quote-btc',
+    name='quote_btc',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'requests>=2.32.0,<3.0.0',
         'certifi~=2025.1.31',
-        'charset-normalizer~gi=3.4.1',
+        'charset-normalizer~=3.4.1',
         'idna~=3.10',
         'urllib3~=2.4.0',
     ],
