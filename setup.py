f5b6f14feb9c3d65f5d0fe80baf0b12a0878ba7f
x <x>
2025-04-12 15:16:02 -0300

    CI/CD next steps: Build process v0.1.0 and docker

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quote_btc',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.32.0,<3.0.0',
+        'certifi~=2025.1.31',
+        'charset-normalizer~=3.4.1',
+        'idna~=3.10',
+        'urllib3~=2.4.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'quote-btc=app.main:main',
+        ],
+    },
+)
